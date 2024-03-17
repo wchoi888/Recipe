1aacfdb23921e2363118e1b13c8e8514b40f5aac
Artemis051 <aekatsi@gmail.com>
2024-03-17 17:46:57 -0400

    now users are able to search an ingredient, like apple, in the search bar, and see various results which they can click on to see more details about. I also removed some of the index.js file to clean it up a little

--- frontend/src/components/ExternalRecipes/ExternalRecipes.jsx
+++ frontend/src/components/ExternalRecipes/ExternalRecipes.jsx
@@ -5,30 +5,31 @@ import { useLocation } from 'react-router-dom';
 
 const ExternalRecipes = () => {//define component to handle search queries
     const [recipes, setRecipes] = useState([]);//create state to store recipes...
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
+    const [mealDetails, setMealDetails] = useState({}); //storing meal details as an object
     const location = useLocation();
 
     const fetchRecipes = (query) => { //functin to fetch recipes
         // if(searchQuery) {//search query is require to make fetch
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)//query based on keyword
-                .then(response => response.json())
-                .then(data => setRecipes(data.meals))
-                .catch(err => console.error(err));
-        
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)//query based on keyword
+            .then(response => response.json())
+            .then(data => setRecipes(data.meals))
+            .catch(err => console.error(err));    
+    };
+
+    const fetchMealDetails = (mealId) => { //function to get meal detials (ingredients, instructions)
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+            .then(response => response.json())
+            .then(data => {
+                setMealDetails(prevDetails => ({
+                    ...prevDetails,
+                    [mealId]: data.meals[0] // Store meal details using mealId as key
+                }));
+            }) 
+            .catch(err => console.error(err));
     };
 
     const handleRecipeSelect = (recipe) => {
-        setSelectedRecipe(recipe);
-    };
-
-    //show recipe details in new section
-    const renderRecipeDetails = () => {
-        return selectedRecipe && (
-            <div>
-                <h2>{selectedRecipe.strMeal}</h2>
-                <img src={selectedRecipe.strMealThumb} alt={selectedRecipe.strMeal} />
-            </div>
-        );
+        fetchMealDetails(recipe.idMeal);
     };
 
     useEffect(() => { //useEffect hook to make side effects in the component
@@ -41,15 +42,17 @@ const ExternalRecipes = () => {//define component to handle search queries
 
     return (
         <div>
-            <div>
-                {recipes && recipes.map((recipe, index) => (
-                    <div key={index} onClick={() => handleRecipeSelect(recipe)}>
-                        <h3>{recipe.strMeal}</h3>
-                        <img src={recipe.strMealThumb} alt={recipe.strMeal} />
-                    </div>
-                ))}
-            </div>
-            {renderRecipeDetails()}
+            {recipes && recipes.map((recipe, index) => (
+                <div key={index}>
+                    <h3 onClick={() => handleRecipeSelect(recipe)}>{recipe.strMeal}</h3>
+                    <img src={recipe.strMealThumb} alt={recipe.strMeal} onClick={() => handleRecipeSelect(recipe)} />
+                    {mealDetails[recipe.idMeal] && ( // Check if details for this mealId exist
+                        <div>
+                            <p>Instructions: {mealDetails[recipe.idMeal].strInstructions}</p>
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
     );
 };
