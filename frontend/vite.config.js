32b08d6aaabb0010bb32aef4a03ec2689b7a330f
Artemis051 <aekatsi@gmail.com>
2024-03-20 23:00:18 -0400

    added function to prevent users from adding a recipe without filling out the recipe form

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         host: true,
         proxy: {
           '/graphql': {
-            target: 'http://localhost:3001', 
+            target: 'http://localhost:3002', 
             secure: false,
             changeOrigin: true,
           }
