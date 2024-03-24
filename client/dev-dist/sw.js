708e1b2ae81e48f401394853bc3310446eb9965b
Artemis051 <aekatsi@gmail.com>
2024-03-24 01:04:29 -0400

    changed port in vite.config to 3005. I tested the app and it seems to work without wifi

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.45pid0rp6f8"
+    "revision": "0.q11qfi9mvqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,76 +1,76 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    //make it mobile friendly
-    port: 5173,
-    open: true,
-    host: true,
-    proxy: {
-      "/graphql": {
-        target: "http://localhost:3005",
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
-});
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { VitePWA } from 'vite-plugin-pwa'
 // // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [react(),VitePWA({
-//     registerType: "autoUpdate",injectRegister:"script-defer",devOptions:{enabled:true},
-//     manifest:{
-//       name:"Recipes for Me",
-//       short_name: "Recipes",
-//       description: "recipe sharing platform",
-//       theme_color:"#ffffff",
-//       icons:[{
-//         src: 'pwa-192x192.png',
-//         sizes: '192x192',
-//         type: 'image/png'
-//       },
-//       {
-//         src: 'pwa-512x512.png',
-//         sizes: '512x512',
-//         type: 'image/png'
-//       }]
-//     },
-//     workbox: {
-//       globPatterns: ['**/*.{js,css,html,png,svg,jpg,pdf,jsx}'],
-//       runtimeCaching: [
-//         {
-//           urlPattern: ({ url }) => {
-//             return url.pathname.startsWith("/src/assets");
-//           },
-//           handler: "CacheFirst",
-//           options: {
-//             cacheName: "assets-cache",
-//             cacheableResponse: {
-//               statuses: [0, 200],
-//             },
-//           },
-//         },
-//       ],
-//     },
-//   })],
-//  server: { //make it mobile friendly
-//     port: 3000,
+//   plugins: [react()],
+//   server: {
+//     //make it mobile friendly
+//     port: 5173,
 //     open: true,
 //     host: true,
 //     proxy: {
-//       '/graphql': {
-//         target: 'http://localhost:3001', //make sure it's the right port, as wing
+//       "/graphql": {
+//         target: "http://localhost:3005",
 //         secure: false,
 //         changeOrigin: true,
-//       }
-//     }
-//   }
-// })
+//       },
+//     },
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),VitePWA({
+    registerType: "autoUpdate",injectRegister:"script-defer",devOptions:{enabled:true},
+    manifest:{
+      name:"Recipes for Me",
+      short_name: "Recipes",
+      description: "recipe sharing platform",
+      theme_color:"#ffffff",
+      icons:[{
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,jpg,pdf,jsx}'],
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => {
+            return url.pathname.startsWith("/src/assets");
+          },
+          handler: "CacheFirst",
+          options: {
+            cacheName: "assets-cache",
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+  })],
+ server: { //make it mobile friendly
+    port: 3000,
+    open: true,
+    host: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3005', //make sure it's the right port, as wing
+        secure: false,
+        changeOrigin: true,
+      }
+    }
+  }
+})
 
