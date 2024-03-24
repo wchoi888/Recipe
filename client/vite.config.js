d1a311c96ddf806ca0174261036e3e82f241df70
Wing Choi <wchoi888@live.com>
2024-03-24 19:12:19 -0400

    PWA changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "script-defer",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "Recipes for Me",
+        short_name: "Recipes",
+        description: "recipe sharing platform",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "./android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "./android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,jpg,pdf,jsx}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/src/assets");
+            },
+            handler: "CacheFirst",
+            options: {
+              cacheName: "assets-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     //make it mobile friendly
     port: 5173,
@@ -74,3 +124,23 @@ export default defineConfig({
 //   }
 // })
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     //make it mobile friendly
+//     port: 5173,
+//     open: true,
+//     host: true,
+//     proxy: {
+//       "/graphql": {
+//         target: "http://localhost:3005",
+//         secure: false,
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
